6b2ff843677badcd8fd025845a127fe6effae0cc
Ángel Viejobueno <168086692+vierodan@users.noreply.github.com>
2026-06-26 21:52:40 +0200

    App setted to port 22000

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 22000,
+    strictPort: true,
+  },
+  preview: {
+    port: 22000,
+    strictPort: true,
+  },
 });
